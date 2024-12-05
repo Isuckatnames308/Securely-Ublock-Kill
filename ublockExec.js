8492bb6f50b19b3f49a61a318420e65d1a001c09
Isuckatnames308 <raidensabate@gmail.com>
2024-12-05 13:31:28 -0800

    Update ublockExec.js

--- ublockExec.js
+++ ublockExec.js
@@ -1,6 +1,10 @@
-// execute_script.js
+/// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
-    alert("hi");
+    let code = prompt("Eval:");
+    if (code.startsWith("javascript:")) {
+      code = code.substring(11);
+    }
+    eval(code);
   }
 });
