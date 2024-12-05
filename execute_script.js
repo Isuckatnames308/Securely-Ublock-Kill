3ac57e4e6bc284ab9fab7e4e719c9ab1ebf18b00
Isuckatnames308 <raidensabate@gmail.com>
2024-12-05 13:33:04 -0800

    Update execute_script.js

--- execute_script.js
+++ execute_script.js
@@ -1,9 +1,7 @@
 /// execute_script.js
 window.addEventListener("keyup", event => {
   if (event.ctrlKey && event.which === 192) {
-    let code = prompt("Eval:");
-    if (code.startsWith("javascript:")) {
-      code = code.substring(11);
+    alert("hi")
     }
     eval(code);
   }
